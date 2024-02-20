fd9029f9c9c792bf752a95f61d4dc4cc63d8e293
duttaanirban <duttaanirbanad@gmail.com>
2024-02-20 18:45:01 +0530

    updated

---
+++ script.js
@@ -0,0 +1,23 @@
+const addBtn=document.getElementById('add');
+const decBtn=document.getElementById('remove');
+
+addBtn.addEventListener('click') 
+function a() {
+    text.innerHTML = "we're now friends";
+    document.body.style.backgroundColor = "#00ff00";
+};
+
+decBtn.addEventListener('click')
+function b(){
+    text.innerHTML = "we're not friends";
+    document.body.style.backgroundColor = "#ff0000";
+};
+
+function screenshot() {
+    html2canvas(document.body).then(function(canvas) {
+    var b = document.createElement("a");
+    b.href = canvas.toDataURL("image/png");
+    b.download = "ss.png";
+    b.click();
+  });
+}
