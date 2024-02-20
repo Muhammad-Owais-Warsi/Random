b71b1daa4f1ac143da46756d3c21c79c4a4f3e22
duttaanirban <duttaanirbanad@gmail.com>
2024-02-20 19:09:36 +0530

    updated1

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const addBtn=document.getElementById('add');
 const decBtn=document.getElementById('remove');
 
-addBtn.addEventListener('click') 
-function a() {
+addBtn.addEventListener('click' , function a() {
     text.innerHTML = "we're now friends";
     document.body.style.backgroundColor = "#00ff00";
-};
+    share.disabled = false;
+});
 
-decBtn.addEventListener('click')
-function b(){
+decBtn.addEventListener('click', function b() {
     text.innerHTML = "we're not friends";
     document.body.style.backgroundColor = "#ff0000";
-};
+    share.disabled = false;
+});
 
 function screenshot() {
     html2canvas(document.body).then(function(canvas) {
